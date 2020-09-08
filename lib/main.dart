584d493b57d3466b9f81ecbc7f84378746aba3ff
sanayut12345 <sanayut.t@live.ku.th>
2020-09-08 00:26:32 -0700

    layout center

--- lib/main.dart
+++ lib/main.dart
@@ -23,35 +23,12 @@ class MyApp extends StatelessWidget {
 class TestWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      alignment: Alignment.topCenter,
-      children: [
-        Container(
-          height: 300.0,
-          width: 300.0,
-          color: Colors.green,
-        ),
-        Container(
-          height: 200.0,
-          width: 200.0,
-          color: Colors.red,
-        ),
-        Container(
-          height: 100.0,
-          width: 100.0,
-          color: Colors.pink,
-        ),
-        Container(
-          height: 50.0,
-          width: 50.0,
-          color: Colors.black,
-        ),
-        Container(
-          height: 25.0,
-          width: 25.0,
-          color: Colors.deepPurple,
-        ),
-      ],
+    return Center(
+      child: Container(
+        height: 200,
+        width: 200,
+        color: Colors.pink,
+      ),
     );
   }
 }
