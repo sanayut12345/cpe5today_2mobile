3fd18c2d2372e996f2baa61ae624698d15214590
sanayut12345 <sanayut.t@live.ku.th>
2020-09-08 00:19:12 -0700

    basic widget stack

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
         body: Container(
            color: Colors.blue,
            child: TestWidget(),
-           height: 500.0,
+           height: 800.0,
            width: 500.0,
         )
       ),
@@ -23,8 +23,35 @@ class MyApp extends StatelessWidget {
 class TestWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Image.network(
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcREcqspi5XGwdEJQttgGmlAxVMb8sazls8vFQ&usqp=CAU",
-      );
+    return Stack(
+      alignment: Alignment.topCenter,
+      children: [
+        Container(
+          height: 300.0,
+          width: 300.0,
+          color: Colors.green,
+        ),
+        Container(
+          height: 200.0,
+          width: 200.0,
+          color: Colors.red,
+        ),
+        Container(
+          height: 100.0,
+          width: 100.0,
+          color: Colors.pink,
+        ),
+        Container(
+          height: 50.0,
+          width: 50.0,
+          color: Colors.black,
+        ),
+        Container(
+          height: 25.0,
+          width: 25.0,
+          color: Colors.deepPurple,
+        ),
+      ],
+    );
   }
 }
